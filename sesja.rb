a0381548e2cfa52bbba90856919d79d05be2f198
Michał Zając <michal.zajac@gmail.com>
2016-01-20 23:13:50 +0100

    My work here is not done

--- sesja.rb
+++ sesja.rb
@@ -8,7 +8,6 @@ class SesjaLinuksowa < Sinatra::Application
 
     register Sinatra::R18n
 
-    set :assets_precompile, %w(application.js application.css *.png *.jpg *.svg *.eot *.ttf *.woff)
     set :assets_css_compressor, :sass
     set :assets_js_compressor, :uglifier
     register Sinatra::AssetPipeline
